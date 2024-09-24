19db034a708bcc45336a7d67948c5c478eed846f
Anisha Shende <91362696+AnishaShende@users.noreply.github.com>
2024-09-24 17:06:21 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import SearchResults from './pages/SearchResults';
-import Home from './pages/Home';
-import CaseVisualization from './pages/RenderGraph';
-
-
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import SearchResults from "./pages/SearchResults";
+import Home from "./pages/Home";
+import CaseVisualization from "./pages/RenderGraph";
 
 function App() {
-  const [searchQuery, setSearchQuery] = useState('');
+  const [searchQuery, setSearchQuery] = useState("");
   return (
-    
-      <div className="font-sans antialiased">
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/search-results" element={<SearchResults searchQuery={searchQuery} />} />
-          <Route path="/visualize/:caseId" component={CaseVisualization} />
-        </Routes>
-      </div>
-    
+    <div className="font-sans antialiased">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route
+          path="/search-results"
+          element={<SearchResults searchQuery={searchQuery} />}
+        />
+        <Route path="/visualize/:caseId" component={CaseVisualization} />
+      </Routes>
+    </div>
   );
 }
 
