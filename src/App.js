e2aaefb001be676220a351b56cb656fc5b521dd3
Anisha Shende <91362696+AnishaShende@users.noreply.github.com>
2024-09-24 16:43:57 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React, { useState } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import SearchResults from './pages/SearchResults';
 import Home from './pages/Home';
-
+import CaseVisualization from './pages/RenderGraph';
 
 
 
@@ -15,6 +15,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/search-results" element={<SearchResults searchQuery={searchQuery} />} />
+          <Route path="/visualize/:caseId" component={CaseVisualization} />
         </Routes>
       </div>
     
